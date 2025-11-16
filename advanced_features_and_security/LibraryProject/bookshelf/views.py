055663c144893cd9df80dc43e89becda27b678e8
Dominic62q <dominicquainoo62@gmail.com>
2025-11-16 16:29:34 +0000

    Add Features and Security

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,8 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
-from .forms import  ExampleForm  
+from .forms import BookForm, CustomUserForm, ExampleForm
 
 # ----------------------------
 # List all books (requires can_view)
