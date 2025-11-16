5d392c19eb72a20586d5189ebd7fca6bd0993c3b
Dominic62q <dominicquainoo62@gmail.com>
2025-11-16 16:21:29 +0000

    Add Features and Security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser, Book
+from .models import Example
+ 
 
 # ----------------------------
 # Custom Forms for Admin
