08410192cbf2b59a13c57ae2db9e77db2b66c85d
Dominic62q <dominicquainoo62@gmail.com>
2025-11-16 14:42:35 +0000

    Add Features and Security

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,17 +1,42 @@
 # LibraryProject/bookshelf/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 from .models import CustomUser, Book
+
+# ----------------------------
+# Custom Forms for Admin
+# ----------------------------
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'date_of_birth', 'profile_photo')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'date_of_birth', 'profile_photo')
 
 # ----------------------------
 # Custom User Admin
 # ----------------------------
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm       # Edit form
+    add_form = CustomUserCreationForm # Creation form using custom manager
+
     fieldsets = UserAdmin.fieldsets + (
         ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
     list_display = ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
 
 # ----------------------------
 # Book Admin
