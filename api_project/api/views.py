22f19c27e8a7894f437a699728106cf1ff00bdcf
Dominic62q <dominicquainoo62@gmail.com>
2025-11-25 09:34:55 +0000

    Add authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,10 +3,16 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.permissions import IsAuthenticated
-
+from rest_framework import generics
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
