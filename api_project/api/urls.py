0247e8b819bd6dd184badefc7e2fd2ac28e97595
Dominic62q <dominicquainoo62@gmail.com>
2025-11-25 09:38:24 +0000

    Update Url

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet
+from .views import BookViewSet, BookList
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
@@ -9,6 +9,7 @@ router.register(r'books', BookViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('books/', BookList.as_view(), name='book-List')
 ]
 
 # Users can obtain a token by POSTing their username and password to /api-token-auth/
