1a2b4a77bbddf0d2c032258914a5b74e5328253c
Dominic62q <dominicquainoo62@gmail.com>
2025-12-07 18:50:39 +0000

    Add comment

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -147,3 +147,18 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
     def get_success_url(self):
         return self.object.post.get_absolute_url()
+    
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+
+    def form_valid(self, form):
+        # Attach post + author
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.object.post.get_absolute_url()
